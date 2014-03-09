f3b8a7fc6525562b54a6dd84a292119e7cfcbc56
Zachary Beyer <zachary@zbeyer.com>
2014-03-08 20:39:02 -0800

    Hello World

--- ios/sandBox/sandBox/PCViewController.m
+++ ios/sandBox/sandBox/PCViewController.m
@@ -20,6 +20,37 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void) viewWillAppear:(BOOL)animated;
+{
+    [super viewWillAppear:animated];
+    [self helloWorld];
+}
+
+- (void) helloWorld;
+{
+    for (UIView * view in self.view.subviews)
+    {
+        [view removeFromSuperview];
+    }
+    
+    float width = self.view.frame.size.width/2;
+    float height = self.view.frame.size.height/2;
+    
+    CGRect r = CGRectMake((self.view.frame.size.width - width)/2,
+                          (self.view.frame.size.height - height)/2,
+                          width,
+                          height);
+    
+    UILabel * hWorld = [[UILabel alloc]initWithFrame:r];
+    hWorld.backgroundColor = [UIColor clearColor];
+    hWorld.text = @"Hello World";
+    hWorld.font = [UIFont fontWithName:hWorld.font.familyName size:64];
+    hWorld.adjustsFontSizeToFitWidth = YES;
+    hWorld.textAlignment = NSTextAlignmentCenter;
+    hWorld.textColor = [[UIColor blackColor]colorWithAlphaComponent:0.5];
+    [self.view addSubview:hWorld];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
